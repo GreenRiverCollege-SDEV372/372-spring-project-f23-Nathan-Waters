d22e42b22286db897b86d9c11bb25591c8cb62f3
NathanWaters <nathanwaters94@gmail.com>
2024-01-13 07:20:29 -0800

    Add Initial project

---
+++ src/main/java/edu/greenriver/sdev/food/FoodApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApplication.class, args);
+	}
+ //hello git!
+}
