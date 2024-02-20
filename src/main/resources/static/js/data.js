527315d3722aa580c10dd46c4d2209c784ef3548
Nathan Waters <waters.nathan@student.greenriver.edu>
2024-02-19 19:17:26 -0800

    ability to async add recipes to the list added.

--- src/main/resources/static/js/data.js
+++ src/main/resources/static/js/data.js
@@ -7,6 +7,16 @@ window.onload = async function() {
     const response = await fetch(uri, config);
     const data = await response.json();
     makeTable(data);
+
+    const addButton = document.querySelector("#submit");
+    addButton.onclick = addRecipe;
+
+    const addMethodButton = document.querySelector("#addMethod");
+    addMethodButton.onclick = addMethod;
+
+    const addIngButton = document.querySelector("#addIngredient");
+    addIngButton.onclick = addIngredient;
+
 }
 
 function makeTable(recipeData){
@@ -19,17 +29,71 @@ function makeTable(recipeData){
     }
 }
 
+async function addRecipe(e){
+    e.preventDefault();
+
+    const newRecipe = {
+        name: document.querySelector("#name").value,
+        url: document.querySelector("#url").value,
+        author: document.querySelector("#author").value,
+        ingredients: Array.from(document.querySelectorAll(".ingredient-input")).map(input => input.value),
+        method: Array.from(document.querySelectorAll(".method-input")).map(input => input.value)
+    }
+
+    console.log(newRecipe);
+
+    const uri = "http://localhost:8080/recipes";
+    const config = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newRecipe)
+    }
+
+    const response = await fetch(uri, config);
+    const recipe = await response.json();
+
+    const row = document.createElement('tr');
+    const tBody = document.querySelector("#tbody");
+    addRecipeToTable(row, recipe, tBody);
+}
+
 function addRecipeToTable(row, recipeData, tBody){
-    const ingredientsList = recipeData.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
-    const methodList = recipeData.method.map(step => `<li>${step}</li>`).join('');
+    // let ingredientsListHTML = '<ul>';
+    // for (let i = 0; i < recipeData.ingredients.length; i++) {
+    //     ingredientsListHTML += `<li>${recipeData.ingredients[i]}</li>`;
+    // }
+    // ingredientsListHTML += '</ul>';
 
     row.innerHTML += `
             <td>${recipeData.name}</td>
-            <td><ul>${ingredientsList}</ul></td>
-            <td><ul>${methodList}</ul></td>
+            <td>${recipeData.ingredients}</td>
+            <td>${recipeData.method}</td>
             <td>edit</td>
             <td>delete</td>
         `;
     tBody.appendChild(row);
 }
 
+function addIngredient() {
+    const ingredientsInputs = document.getElementById('ingredientsInputs');
+
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = 'ingredients[]';
+    newInput.className = 'ingredient-input';
+
+    ingredientsInputs.appendChild(newInput);
+}
+
+function addMethod() {
+    const methodInputs = document.getElementById('methodInputs');
+
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = 'method[]';
+    newInput.className = 'method-input';
+
+    methodInputs.appendChild(newInput);
+}
